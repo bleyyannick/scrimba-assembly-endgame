b4686b0b84f22726e645ca2adb083091d5abb895
Yannick Bley <bleyyannickpro@gmail.com>
2024-12-09 13:03:27 +0100

    feat: add languages chips to UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Header from './Header'
+import { languages } from './languages'
 
 export default function Hangman() {
+  const languagesChips = languages.map(({backgroundColor, color, name}) => 
+    <div key={name} className="chips"style={{backgroundColor,color}}>
+       {name}
+    </div>
+)
     return (
         <main>
            <Header />
+           <section>
+                <p>You Win </p>
+                <span>Well Done</span>
+            </section>
+            <section className="container-chips">
+              {languagesChips}
+            </section>
         </main>
     )
 }
